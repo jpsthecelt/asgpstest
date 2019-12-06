158497ad933cf4c63ea1c6a04468a092a3c5408d
John Singer <wz3264@ad.csueastbay.edu>
2019-12-06 09:21:33 -0800

    Converging flaskExp/quartExp/as_GPS into avlocator

---
+++ serialAsyncLoop.py
@@ -0,0 +1,25 @@
+import asyncio
+import serial
+
+s = serial.Serial('/dev/pts/13', 9600)
+
+def test_serial():
+    '''
+    read a line and print.
+    '''
+    text = ""
+    msg = s.read().decode()
+    while (msg != '\n'):
+        text += msg
+        msg = s.read().decode()
+    print(text)
+    loop.call_soon(s.write, "ok\n".encode()) 
+
+loop = asyncio.get_event_loop()
+loop.add_reader(s, test_serial)
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    loop.close()
